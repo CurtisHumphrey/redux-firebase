861ff3ecb216f39756b7e35d339bff3d8883235d
Curtis M. Humphrey, Ph.D <curtis@createdwithflair.com>
2020-11-04 08:41:13 -0600

    fix: order should be next then set so that redux lists the order correctly

--- src/middleware.js
+++ src/middleware.js
@@ -1,30 +1,44 @@
-import Firebase from 'firebase/app'
+import Firebase from "firebase/app";
 
-import {auth_listener} from './app_listeners'
-import action_handlers from './action_handlers'
+import { auth_listener } from "./app_listeners";
+import action_handlers from "./action_handlers";
 
 const firebase_exchanger = (store) => {
-  auth_listener(store.dispatch, store.getState)
+  auth_listener(store.dispatch, store.getState);
 
   return (next) => (action) => {
     if (action_handlers[action.type] != null) {
-      const response = action_handlers[action.type](action)(store.dispatch, store.getState)
-      next(action)
-      return response
+      switch (action.type) {
+        case "firebase/on":
+        case "firebase/switch":
+          // setup handlers before saving their paths
+          const response = action_handlers[action.type](action)(
+            store.dispatch,
+            store.getState
+          );
+          next(action);
+          return response;
+        default:
+          next(action);
+          return action_handlers[action.type](action)(
+            store.dispatch,
+            store.getState
+          );
+      }
     }
-    return next(action)
-  }
-}
+    return next(action);
+  };
+};
 
 // middleware maker
 export default (config) => {
   try {
-    Firebase.initializeApp(config)
+    Firebase.initializeApp(config);
   } catch (err) {} // silence reinitialize warning (hot-reloading)
 
   if (config.enableLogging && __DEV__) {
-    Firebase.database.enableLogging(config.enableLogging)
+    Firebase.database.enableLogging(config.enableLogging);
   }
 
-  return firebase_exchanger
-}
+  return firebase_exchanger;
+};
